f29f13bb48db1bb2649e685cb0e23f7dfe6b3faf
James Golding <james@goldi.ng>
2022-07-27 15:39:47 +0100

    Project completed and working

--- day-017/main.py
+++ day-017/main.py
@@ -1,13 +1,20 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 
 question_bank = []
 
-for i in range(len(question_data)):
-    q = ""
-    a = ""
-    for key, value in question_data[i]:
-        q = key
-        a = value
-        print(f"question: {q} ||| answer: {a}")
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You have completed the quiz.")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
