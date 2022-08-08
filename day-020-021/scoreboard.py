4f9d3181e258346bcd4ea3f87f22145a5a65a882
James Golding <james@goldi.ng>
2022-08-08 13:34:07 +0100

    High score is now saved

--- day-020-021/scoreboard.py
+++ day-020-021/scoreboard.py
@@ -9,12 +9,20 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.highscore = 0
+        self.highscore = self.get_highest_score()
         self.color("white")
         self.penup()
         self.hideturtle()
         self.goto(0, 275)
         self.refresh_scoreboard()
+
+    def get_highest_score(self):
+        with open("data.txt", mode="r") as file:
+            score = file.read()
+            if score:
+                return int(score)
+            else:
+                return 0
 
     def refresh_scoreboard(self):
         self.clear()
@@ -24,12 +32,13 @@ class Scoreboard(Turtle):
         self.score += 1
         self.refresh_scoreboard()
 
+    def save_highest_score(self):
+        with open("data.txt", mode="w") as data:
+            data.write(str(self.highscore))
+
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            self.save_highest_score()
         self.score = 0
         self.refresh_scoreboard()
-
-    # def game_over(self):
-    #     self.goto(0, 0)
-    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
