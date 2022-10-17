33528ec3d9f1a40c5f1797242d6034efede2dd22
James Golding <james@goldi.ng>
2022-10-17 14:29:33 +0700

    Project completed and working

--- day-031/main.py
+++ day-031/main.py
@@ -4,19 +4,33 @@ import pandas
 
 BACKGROUND_COLOR = "#B1DDC6"
 
-file = pandas.read_csv("data/french_words.csv")
-data = pandas.DataFrame.to_dict(file, orient="records")
+# Read from words_to_learn if it exists, else read from french_words
+try:
+    file = pandas.read_csv("data/words_to_learn.csv")
+except (FileNotFoundError, pandas.errors.EmptyDataError):
+    file = pandas.read_csv("data/french_words.csv")
+to_learn = pandas.DataFrame.to_dict(file, orient="records")
 current_card = {}
 
 
 def next_flashcard():
     global current_card, flip_timer
     window.after_cancel(flip_timer)
-    current_card = random.choice(data)
+    current_card = random.choice(to_learn)
     canvas.itemconfig(card_title, text="French", fill="black")
     canvas.itemconfig(card_word, text=current_card["French"], fill="black")
     canvas.itemconfig(card_background, image=front_flash_card)
     flip_timer = window.after(3000, func=flip_card)
+
+
+# If player knew the answer, remove it from the list
+def remove_then_next_flashcard():
+    to_learn.remove(current_card)
+    if len(to_learn) == 0:
+        canvas.itemconfig(card_word, text="NO MORE WORDS!")
+    listy_csv = pandas.DataFrame(to_learn)
+    listy_csv.to_csv("data/words_to_learn.csv", index=False)
+    next_flashcard()
 
 
 def flip_card():
@@ -53,7 +67,7 @@ wrong_button.grid(column=0, row=1)
 # Right button
 
 tick_image = PhotoImage(file="images/right.png")
-right_button = Button(image=tick_image, highlightthickness=0, command=next_flashcard)
+right_button = Button(image=tick_image, highlightthickness=0, command=remove_then_next_flashcard)
 right_button.grid(column=1, row=1)
 
 
