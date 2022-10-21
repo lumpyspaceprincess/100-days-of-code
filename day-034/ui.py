d2aca625156f16c0c7f613e7ec71bee9a52da237
James Golding <james@goldi.ng>
2022-10-21 16:53:27 +0700

    Updated UI for clarity

--- day-034/ui.py
+++ day-034/ui.py
@@ -11,8 +11,22 @@ class QuizInterface:
         self.window.title("Quizzler")
         self.window.config(background=THEME_COLOR, padx=20, pady=20)
 
-        self.canvas = Canvas(width=300, height=250)
-        self.canvas.grid(column=0, row=1, columnspan=2)
+        # Create score UI
+
+        self.score = Label(text="Score: 0", fg="white", background=THEME_COLOR)
+        self.score.grid(column=1, row=0)
+
+        # Create canvas for questions
+
+        self.canvas = Canvas(width=300, height=250, background="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            text="quote goes here",
+            font=FONT,
+            fill=THEME_COLOR
+        )
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
 
         # Create buttons
 
@@ -21,20 +35,10 @@ class QuizInterface:
 
         self.true_button = Button(image=true_image)
         self.true_button.grid(column=0, row=2)
-        self.true_button.config(highlightthickness=0, background=THEME_COLOR, padx=20, pady=20)
+        self.true_button.config(highlightthickness=0, background=THEME_COLOR)
 
         self.false_button = Button(image=false_image)
         self.false_button.grid(column=1, row=2, )
         self.false_button.config(highlightthickness=0, background=THEME_COLOR)
-
-        # Create score UI
-
-        self.score = Label(text="Score: 0", fg="white")
-        self.score.config(background=THEME_COLOR)
-        self.score.grid(column=1, row=0)
-
-        # Create text on canvas
-
-        self.canvas.create_text(150, 100, text="quote goes here", font=FONT)
 
         self.window.mainloop()
