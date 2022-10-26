6de7f008ad3acb6b1939342db9655a67be7039ed
James Golding <james@goldi.ng>
2022-10-26 17:21:59 +0700

    Quantity parameter now takes input from user to update the current day's total

--- day-037/main.py
+++ day-037/main.py
@@ -8,6 +8,7 @@ load_dotenv()
 pixela_endpoint = "https://pixe.la/v1/users"
 PIXELA_TOKEN = os.environ.get("PIXELA_API_TOKEN")
 PIXELA_USERNAME = os.environ.get("PIXELA_USERNAME")
+time_programming_today = input("How many minutes programming did you achieve today?")
 
 pixela_parameters = {
     "username": PIXELA_USERNAME,
@@ -37,12 +38,13 @@ graph_config = {
 # response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 # print(response.text)
 
-today = datetime(year=2022, month=10, day=24)
-the_date = today.strftime("%Y%m%d")
+# what_day = datetime(year=2022, month=10, day=24)
+# the_date = what_day.strftime("%Y%m%d")
+the_date = datetime.now().strftime("%Y%m%d")
 
 post_config = {
     "date": the_date,
-    "quantity": "33",
+    "quantity": time_programming_today,
 }
 
 # To add new information to an existing graph:
@@ -50,13 +52,16 @@ post_config = {
 # print(response.text)
 
 put_config = {
-    "quantity": "60",
+    "quantity": time_programming_today,
 }
 
 # To update an existing entry in an existing graph:
-# response = requests.put(url=f"{graph_endpoint}/{graph_config['id']}/{the_date}",
-#                         json=put_config, headers=headers)
-# print(response.text)
+# PUT requests will also create an entry where one doesn't exist, therefore POST seems redundant. Maybe this is only
+# for Pixela
+response = requests.put(url=f"{graph_endpoint}/{graph_config['id']}/{the_date}",
+                        json=put_config, headers=headers)
+print(response.text)
+print(response.status_code)
 
 # To delete an existing entry in an existing graph:
 # response = requests.delete(url=f"{graph_endpoint}/{graph_config['id']}/{the_date}", headers=headers)
