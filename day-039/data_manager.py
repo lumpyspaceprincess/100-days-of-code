545b08f8484159e5698defab5dbe6b79a6a86c07
James Golding <james@goldi.ng>
2022-12-19 16:36:26 +0900

    Attempting to GET data from sheety

--- day-039/data_manager.py
+++ day-039/data_manager.py
@@ -1,3 +1,27 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SHEETY_PRICES_ENDPOINT = os.environ.get("SHEETY_FLIGHTS")
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    # This class is responsible for talking to the Google Sheet.
+
+    def __init__(self):
+        self.sheet_data = list
+
+    def get_the_data(self):
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        response.raise_for_status()
+        stuff = response.json()
+
+        for item in stuff["prices"]:
+            self.sheet_data.append(item)
+
+        print(self.sheet_data)
+
+
+DataManager.get_the_data()
